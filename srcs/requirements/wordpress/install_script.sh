b72b144050784737786dd8d350ac636c0c08673c
Antoine Labalette <labalette.antoine@gmail.com>
2022-06-27 11:47:53 +0200

    basic theme

--- srcs/requirements/wordpress/install_script.sh
+++ srcs/requirements/wordpress/install_script.sh
@@ -14,6 +14,6 @@ if [ ! -f /var/www/html/wp-config.php ]; then
     mv /var/www/wp-config.php /var/www/html/
     wp core install --allow-root --url=${URL} --title="wordpress" --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PWD} --skip-email --admin_email="labalette.antoine@gmail.com"
     wp user create --allow-root ${DB_USER} "random@gmail.com" --user_pass=${DB_USER_PWD};
-    wp theme install inspiro --activate --allow-root
+    # wp theme install inspiro --activate --allow-root
 fi
 php-fpm7.3 -F
