782a08c69d6abd6a202b174bae77cf8bb3340c96
Antoine Labalette <labalette.antoine@gmail.com>
2022-06-28 17:00:16 +0200

    test fix with psw

--- srcs/requirements/mariadb/start.sh
+++ srcs/requirements/mariadb/start.sh
@@ -5,6 +5,7 @@ if [ ! -f /var/www/old_db.sql ]; then
     service mysql start;
     mysql -u root --password=root12345 < /var/www/initial_db2.sql;
     mv /var/www/initial_db2.sql /var/www/old_db.sql;
+    service mysql stop;
 fi
 
 echo "wordpress database already intialized";
