c3ab29d2082d46c122ce6191191851e23510abae
Antoine Labalette <labalette.antoine@gmail.com>
2022-06-28 23:04:26 +0200

    pwd env

---
+++ srcs/requirements/mariadb/initial_db.sh
@@ -0,0 +1,6 @@
+cat << EOF > initial_db2.sql
+CREATE DATABASE IF NOT EXISTS wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+GRANT ALL ON wordpress.* TO 'alabalet'@'%' IDENTIFIED BY '$DB_USER_PWD';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ADMIN_PWD';
+FLUSH PRIVILEGES;
+EOF
