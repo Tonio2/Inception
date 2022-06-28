07e5cfd03831d4ea75e4a5a92ebe330e8e4580b4
Antoine Labalette <labalette.antoine@gmail.com>
2022-06-28 16:43:37 +0200

    [m] db persistence

--- srcs/requirements/mariadb/start.sh
+++ srcs/requirements/mariadb/start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 if [ -f /var/www/old_db.sql ]; then
-    echo "Initializing wordpress database"
+    echo "Initializing wordpress database";
     service mysql start;
     mysql < /var/www/initial_db2.sql;
     mv /var/www/initial_db2.sql /var/www/old_db.sql;
 fi
 
-echo "wordpress database already intialized"
+echo "wordpress database already intialized";
+
+mysqld;
