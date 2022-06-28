84bf7ca9887af2fdb56f39196ec5cf17ecf9b46f
Antoine Labalette <labalette.antoine@gmail.com>
2022-06-28 23:35:14 +0200

    test mariadb

--- srcs/requirements/mariadb/initial_db.sh
+++ srcs/requirements/mariadb/initial_db.sh
@@ -3,7 +3,8 @@
 echo test
 cat << EOF > /var/www/initial_db2.sql
 CREATE DATABASE IF NOT EXISTS wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-GRANT ALL ON wordpress.* TO 'alabalet'@'%' IDENTIFIED BY '$DB_USER_PWD';
+CREATE USER 'alabalet'@'%' IDENTIFIED BY '$DB_USER_PWD';
+GRANT ALL ON wordpress.* TO 'alabalet'@'%';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ADMIN_PWD';
 FLUSH PRIVILEGES;
 EOF
