46003ed517ef9025b2041e36e78d4a9c53c99ddf
Antoine Labalette <labalette.antoine@gmail.com>
2022-06-29 11:10:30 +0200

    [m] ftp server

--- srcs/requirements/bonus/ftp/start.sh
+++ srcs/requirements/bonus/ftp/start.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-if [ ! -f "/etc/vsftpd/vsftpd.oldconf" ]; then
+if [ ! -f "/etc/vsftpd.oldconf" ]; then
 
     mkdir -p /var/www/html
 
-    cp /etc/vsftpd/vsftpd.conf /etc/vsftpd/vsftpd.oldconf
-    mv /tmp/vsftpd.conf /etc/vsftpd/vsftpd.conf
+    cp /etc/vsftpd.conf /etc/vsftpd.oldconf
+    mv /tmp/vsftpd.conf /etc/vsftpd.conf
 
     # Add the FTP_USER, change his password and declare him as the owner of wordpress folder and all subfolders
     adduser $FTP_USR --disabled-password
@@ -17,4 +17,4 @@ if [ ! -f "/etc/vsftpd/vsftpd.oldconf" ]; then
 fi
 
 echo "FTP started on :21"
-/usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
+/usr/sbin/vsftpd /etc/vsftpd.conf
